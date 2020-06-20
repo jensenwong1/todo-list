58aad2ce5f6d6927aa9497c361cf247dbf1e9719
Jensen Wong <jensenwong1@gmail.com>
2020-06-20 10:56:25 -0700

    2nd commit.

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var items=["Buy food","Cook food", "Eat food"];
-
+var items = ["Buy food", "Cook food", "Eat food"];
+var workItems = [];
 app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-
-    var today = new Date();
- 
-    var options={
-        weekday:"long",
-        day:"numeric",
-        month:"long",
-    };
-
-    var day=today.toLocaleDateString("en-US", options);
+app.get("/", function(req, res) {
 
 
-    res.render("list", { 
-        kindOfDay: day,
-        newListItems:items,
+    res.render("list", {
+        listTitle: day,
+        newListItems: items,
     });
 
 });
 
-app.post("/", function(req, res){
-   var item=req.body.newItem;
-   items.push(item);
+app.post("/", function(req, res) {
+    let item=req.body.newItem;
 
-    res.redirect("/");
+    if (req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+   })
+
+app.get("/work", function(req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems,
+    });
+
+});
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 })
 
+app.get("/about", function(req, res){
+    res.render("about");
+});
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server is running on local host");
 });
